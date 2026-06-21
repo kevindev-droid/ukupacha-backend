3a5954485cc71ba4684e782de585271f23ee34bc
kevindev-droid <elarrasador100@gmail.com>
2026-06-21 17:18:28 -0500

    fix: escuchar en todas las interfaces para Railway

--- index.js
+++ index.js
@@ -63,6 +63,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
